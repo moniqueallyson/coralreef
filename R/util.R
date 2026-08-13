c65f63cf27a3deabc45b17914ce1aeb71c0845e6
Monique Allyson <moniqueallysonhernandez@gmail.com>
2026-08-13 16:02:06 -0700

    Fix #2

--- R/util.R
+++ R/util.R
@@ -24,3 +24,16 @@ for (rc in random_coral){
 }
 initialize_reef(fill = "<3")
 
+
+# Added the growth function 
+growth <- function(reef, roll, row, col) {
+  growth_row_offset <- c(-1, -1, -1, 0, 1, 1, 1, 0)
+  growth_col_offset <- c(-1, 0, 1, 1, 1, 0, -1, -1)
+
+  growth_row <- row + growth_row_offset[roll]
+  growth_col <- col + growth_col_offset[roll]
+
+  reef[growth_row, growth_col] <- 1
+
+  return(reef)
+}
--- initalize_reef.R
+++ initalize_reef.R
@@ -3,12 +3,14 @@ reef_init<- matrix(data = 0, nrow = 5, ncol = 5)
 reef_init
 random <- sample(1:25, size = 8, replace = FALSE)
 1:25
+
 # Rows
 (1:25) %% 5
-(1:25 - 1) %% 5+1
+(1:25 - 1) %% 5
+(1:25 - 1) %% 5 + 1
 
 # columns
-1:25 %/% 5
+(1:25) %/% 5
 (1:25 - 1) %/% 5 + 1
 
 reef <- matrix(0, nrow = 5, ncol = 5)
@@ -17,4 +19,10 @@ cell_row <- (6-1) %/% 5 + 1
 cell_col <- (6-1) %% 5 + 1
 reef[cell_row, cell_col] <- 1
 reef
-cell_label[1]
+
+
+cell_label <- 16
+cell_row <- (16 - 1) %/% 5 + 1
+cell_col <- (16-1) %% 5 + 1
+reef[cell_row, cell_col] <- 1
+reef
