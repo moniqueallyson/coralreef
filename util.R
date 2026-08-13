975a7ab8e407a8d68b76e804cc10fe3977543b2b
Monique Allyson <moniqueallysonhernandez@gmail.com>
2026-08-13 15:54:05 -0700

    util R

--- util.R
+++ util.R
@@ -1,8 +1,18 @@
+# 8 random coral : Serious Ver.
+initialize_reef <- function(num_coral){
+reef <- matrix(0, nrow = 5, ncol = 5)
+random_coral<- sample(1:25, size = num_coral, replace = FALSE)
+for (rc in random_coral){
+  rc_row <- (rc - 1) %/% 5 + 1
+  rc_col <-  (rc - 1) %% 5 + 1
+  reef[rc_row, rc_col] <- 1 
+}
+  return(reef)
+}
+initialize_reef(8)
 
-
-# 8 random coral
-
-nitialize_reef <- function(num_coral = 8, fill = 1){
+# 8 random coral : KAWAII Ver.
+initialize_reef <- function(num_coral = 8, fill = 1){
 reef <- matrix(0, nrow = 5, ncol = 5)
 random_coral<- sample(1:25, size = num_coral, replace = FALSE)
 for (rc in random_coral){
@@ -13,3 +23,4 @@ for (rc in random_coral){
   return(reef)
 }
 initialize_reef(fill = "<3")
+
